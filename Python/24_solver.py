4b4e0e231845b4e4da85449bd8fea96b615efe54
SohumKaji <sohumkaji@gmail.com>
2017-09-28 09:14:38 -0400

    Reformatted 24_Solver Comments

--- Python/24_solver.py
+++ Python/24_solver.py
@@ -4,12 +4,12 @@
 # Given 4 digits, searching the solution space for values that calculate to 24 using only +,-,*,/ #
 ###################################################################################################
 
-															#Python doesn't support tail-call 
-from copy import copy													# optimization. So these types of problems 
-import sys														# would be better solved with iteration.
- 															# Ignoring that fact, we can increase the 					
-sys.setrecursionlimit(1500)												# maximum depth recursion limit so that the 
-															# optimization is not an issue for our needs
+							#Python doesn't support tail-call 
+from copy import copy					# optimization. So these types of problems 
+import sys						# would be better solved with iteration.
+ 							# Ignoring that fact, we can increase the 					
+sys.setrecursionlimit(1500)				# maximum depth recursion limit so that the 
+							# optimization is not an issue for our needs
 
 
 
@@ -43,9 +43,9 @@ def is24(nums, operations):
 	ans = float(nums[0]) 
 	operations = list(operations)											
 	
-	for elem, oper in zip(nums[1:], operations):									#Check if this particular ordering of the
-		if(oper == '3'): ans += float(elem)									# input with this particular ordering of the
-		elif(oper == '2'): ans -= float(elem)									# operators, yields 24
+	for elem, oper in zip(nums[1:], operations):			#Check if this particular ordering of the
+		if(oper == '3'): ans += float(elem)			# input with this particular ordering of the
+		elif(oper == '2'): ans -= float(elem)			# operators, yields 24
 		elif(oper == '1'): ans *= float(elem)
 		elif(oper == '0'): 
 			if(elem != 0): ans /= float(elem)
@@ -53,17 +53,17 @@ def is24(nums, operations):
 	
 	return ans == 24
 
-def shuffle(nums, nums_list):												#Shuffle the input numbers
+def shuffle(nums, nums_list):						#Shuffle the input numbers
 	iter = len(nums_list) % 6
-															#This follows the pattern:
-	if(iter == 0): nums[0], nums[3] = nums[3], nums[0]								# 1, 2, 3, 4
-	if(iter == 1): nums[2], nums[3] = nums[3], nums[2]								# 1, 2, 4, 3
-	if(iter == 2): nums[1], nums[3] = nums[3], nums[1]								# 1, 3, 4, 2
-	if(iter == 3): nums[2], nums[3] = nums[3], nums[2]								# 1, 3, 2, 4
-	if(iter == 4): nums[1], nums[3] = nums[3], nums[1]								# 1, 4, 2, 3
-	if(iter == 5): nums[2], nums[3] = nums[3], nums[2]								# 1, 4, 3, 2
-															#Exhausting all combinations of 1 first
-															#The first if will now put 2 first
+									#This follows the pattern:
+	if(iter == 0): nums[0], nums[3] = nums[3], nums[0]		# 1, 2, 3, 4
+	if(iter == 1): nums[2], nums[3] = nums[3], nums[2]		# 1, 2, 4, 3
+	if(iter == 2): nums[1], nums[3] = nums[3], nums[1]		# 1, 3, 4, 2
+	if(iter == 3): nums[2], nums[3] = nums[3], nums[2]		# 1, 3, 2, 4
+	if(iter == 4): nums[1], nums[3] = nums[3], nums[1]		# 1, 4, 2, 3
+	if(iter == 5): nums[2], nums[3] = nums[3], nums[2]		# 1, 4, 3, 2
+									#Exhausting all combinations of 1 first
+									#The first if will now put 2 first
 	return nums
 
 ###########
@@ -73,13 +73,13 @@ def shuffle(nums, nums_list):												#Shuffle the input numbers
 answers = []; no_ans = []; total = 0; ans_string = ''
 nums = ''
 
-while(len(nums) !=4 or not nums.isdigit()): 										#Check input
-	nums = raw_input("Enter 4 numbers to calculate 24 from (Eg. 1234): ")						#Take input
+while(len(nums) !=4 or not nums.isdigit()): 						#Check input
+	nums = raw_input("Enter 4 numbers to calculate 24 from (Eg. 1234): ")		#Take input
 nums = [int(elem) for elem in nums]
 
-answers = recursive_calc(nums)												#Find Answers
+answers = recursive_calc(nums)								#Find Answers
 
-for e in answers:													#Format and print Solutions
+for e in answers:									#Format and print Solutions
 	ans_string ='((('+str(e[0][0])
 	for i, ii in zip(e[0][1:], e[1]):
 		if(ii == '3'): ans_string += '+'
@@ -90,4 +90,4 @@ for e in answers:													#Format and print Solutions
 
 	print ans_string + ' = 24'
 
-if( answers == []): print "\nSorry, no solutions! :-(\n"								#If there are no Solutions
+if( answers == []): print "\nSorry, no solutions! :-(\n"				#If there are no Solutions
